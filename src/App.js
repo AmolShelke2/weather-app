274e4b8448a62b9e91d3a74060224c380c2b9add
AmolShelke2 <shelkeamol897@gmail.com>
2024-04-10 00:22:09 +0530

    configured tailwind css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
-  );
+export default function App() {
+  return <div></div>;
 }
-
-export default App;
---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const MainContainer = () => {
+  return (
+    <div className="bg-white h-screen w-full">
+      {/* search and menu icon */}
+      <div className="flex justify-between items-center"></div>
+    </div>
+  );
+};
